23190aa6d0f118633bcfb9f96da3643f2f37f9e4
sabrina <sabrinaabrodi20@gmail.com>
2022-06-16 09:45:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+
+btn.addEventListener("click", () => {
+  fetch("https://animechan.vercel.app/api/random")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      result.innerHTML = `
+        <h3>${data.anime}</h3>
+        <h2>  ${data.character} </h2>
+        <p>"
+ ${data.quote}
+        "</p>`;
+    });
+});
+
+// btn.addEventListener("click", () => {
+//   fetch("https://animechan.vercel.app/api/random")
+//     .then((response) => response.json())
+//     .then((quote) => console.log(quote));
+// });
